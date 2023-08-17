11889fc576eca505e56293d4a64bb27e824da193
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-08-16 21:11:47 -0600

    Ejercicio 1

---
+++ ejercicio 1/ejercicio 1/Program.cs
@@ -0,0 +1,26 @@
+﻿//EJERCICIO 1
+
+int TAMAÑO;
+do
+{
+
+    Console.Write("ingresa el tamaño del arreglo (minimo 5)>> ");
+
+}
+
+while (!int.TryParse(Console.ReadLine(), out TAMAÑO) || TAMAÑO < 5);
+
+double[] stock = new double[TAMAÑO];
+
+for (int n = 0; n < stock.Length ; n++)
+{
+    Console.Write($"Ingresa el valor para la posicón {n +1}: ");
+    stock[n] = double.Parse(Console.ReadLine());
+}
+
+Console.Write("Valores ingresados: ");
+foreach (float n in stock)
+{
+    Console.Write($" >>{n}");
+}
+
