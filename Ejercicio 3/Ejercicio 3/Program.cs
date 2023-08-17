19dfba636b598832ebeb02338866a8a02dbfc771
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-08-16 22:27:59 -0600

    ejercicio 3

---
+++ Ejercicio 3/Ejercicio 3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Ejercicio 3
+
+using System.Threading.Tasks;
+string[] word = {"Perro", "Casa", "Cebra", "Tortuga",
+"Burro", "Mar", "Dedo", "Tasa", "Nube", "Sol"};
+
+Console.WriteLine("Arreglo sin ordenar: ");
+for(int i = 0; i < word.Length; i++)
+{
+    Console.WriteLine($"Posición {i + 1}: {word[i]}");
+}
+
+Array.Sort(word);
+Console.WriteLine("\n Arreglo en orden: ");
+
+foreach (string i in word)
+{
+    Console.WriteLine(i);
+}
