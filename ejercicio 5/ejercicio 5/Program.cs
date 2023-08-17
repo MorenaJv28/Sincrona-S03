0feb3851527bd555ff98719bfb443c4dfed984ce
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-08-16 22:56:03 -0600

    ejercicio 5

---
+++ ejercicio 5/ejercicio 5/Program.cs
@@ -0,0 +1,25 @@
+﻿//Ejercicio 5
+
+int[] NUM = { 4, 10, 45, -67, -7, 25, 89, 64 };
+
+Console.WriteLine("Arreglo en orden original");
+
+foreach (int n in NUM)
+{
+    Console.WriteLine(n);
+}
+
+Array.Reverse(NUM);
+Console.WriteLine("\nArreglo con Reverse");
+
+foreach (int n in NUM)
+{
+    Console.WriteLine(n);
+}
+
+Array.Sort(NUM);
+Console.WriteLine("\nArreglo con Sort");
+foreach (int n in NUM)
+{
+    Console.WriteLine(n);
+}
