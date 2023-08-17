d129e8adce62a3cba1ce31b98f90d607f7d85aa8
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-08-16 21:53:40 -0600

    Ejercicio 2

---
+++ Ejercicio 2/Ejercicio 2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Ejercicio 2
+
+using System.ComponentModel.DataAnnotations;
+
+string[] NombreComplet = { "Morena", "Beralia", "Gutierrez", "Jovel" };
+
+Console.WriteLine("Valores de Arreglo: ");
+
+foreach (var i in NombreComplet)
+{
+    Console.WriteLine(i);
+}
+
+int TamañoA = NombreComplet.Length;
+
+Console.WriteLine($"\nTamaño del arreglo: {TamañoA}");
+
+Console.WriteLine($"\nTamaño del arreglo y valores: ");
+
+for (int i = 0; i < TamañoA; i++)
+{ 
+    Console.WriteLine($"Posición {i+1}: {NombreComplet[i]}");
+}
+
+
