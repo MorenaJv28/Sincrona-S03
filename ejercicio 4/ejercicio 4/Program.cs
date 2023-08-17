a44890a02904238d346694b37d7336080edc58d7
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-08-16 22:48:50 -0600

    ejercicio 4

---
+++ ejercicio 4/ejercicio 4/Program.cs
@@ -0,0 +1,40 @@
+﻿//Ejercicio 3
+
+using System.Threading.Tasks;
+string[] word = {"Perro", "Casa", "Cebra", "Tortuga",
+"Burro", "Mar", "Dedo", "Tasa", "Nube", "Sol"};
+
+Console.WriteLine("Arreglo sin ordenar: ");
+for (int i = 0; i < word.Length; i++)
+{
+    Console.WriteLine($"Posición {i + 1}: {word[i]}");
+}
+
+Array.Sort(word);
+Console.WriteLine("\n Arreglo en orden: ");
+
+foreach (string i in word)
+{
+    Console.WriteLine(i);
+}
+
+//Ejercicio 4
+
+Console.WriteLine("\n Busqueda con BinarySearch");
+
+String[] WordSearch = { "Sol", "Dedo", "Casa" };
+
+foreach (string i in WordSearch)
+{
+    int posición = Array.BinarySearch(word, i);
+
+    if (posición  >= 0)
+    {
+        Console.WriteLine($"La palabra '{i}' se encontro en la posición {posición}");
+    }
+    else
+    {
+        Console.WriteLine($"La palabra '{i}' no se encontro en la posición");
+    }
+    
+}    
